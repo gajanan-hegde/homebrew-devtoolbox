5f89bde8cf435602127a6d618f0da7c4c30ccba2
gajanan-hegde <gajanan.hegde@deliveryhero.com>
2026-03-11 18:53:47 +0100

    Updated the hash of v1.0 release

--- Casks/devtoolbox.rb
+++ Casks/devtoolbox.rb
@@ -1,6 +1,6 @@
 cask "devtoolbox" do
   version "1.0"
-  sha256 "REPLACE_WITH_SHA256_FROM_BUILD_SCRIPT"
+  sha256 "eba36d826ec25e839a9aae29e03a46a973985a72d089773eeb5c8c2276aa9e0a"
 
   url "https://github.com/gajanan-hegde/DevToolbox/releases/download/v#{version}/DevToolbox-#{version}.zip"
   name "DevToolbox"
